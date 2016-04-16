2b58f67279adee911445e02b5b30702e6249cde6
Adam Sadovsky <asadovsky@gmail.com>
2016-04-16 12:31:55 -0700

    add swift dir with initial cocoa framework xcode project

---
+++ swift/CgoSwift/CgoSwift.h
@@ -0,0 +1,19 @@
+//
+//  CgoSwift.h
+//  CgoSwift
+//
+//  Created by Adam Sadovsky on 4/16/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CgoSwift.
+FOUNDATION_EXPORT double CgoSwiftVersionNumber;
+
+//! Project version string for CgoSwift.
+FOUNDATION_EXPORT const unsigned char CgoSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CgoSwift/PublicHeader.h>
+
+
