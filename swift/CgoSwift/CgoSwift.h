b4774b3eb52cb012a54b3b0c7cfe1160025fe8ba
Adam Sadovsky <asadovsky@gmail.com>
2016-04-16 12:37:35 -0700

    add cgo-built archives and header to xcode project, make C headers public in xcode, add simple example

--- swift/CgoSwift/CgoSwift.h
+++ swift/CgoSwift/CgoSwift.h
@@ -1,11 +1,3 @@
-//
-//  CgoSwift.h
-//  CgoSwift
-//
-//  Created by Adam Sadovsky on 4/16/16.
-//
-//
-
 #import <UIKit/UIKit.h>
 
 //! Project version number for CgoSwift.
@@ -14,6 +6,4 @@ FOUNDATION_EXPORT double CgoSwiftVersionNumber;
 //! Project version string for CgoSwift.
 FOUNDATION_EXPORT const unsigned char CgoSwiftVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <CgoSwift/PublicHeader.h>
-
-
+#import "golib.h"
